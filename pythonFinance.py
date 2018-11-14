6746eebccf6183bf73cedf464be11f6a230485b0
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2018-11-14 18:54:16 -0500

    Exported stock data to CSV format for future reading. Began plotting stock data

--- pythonFinance.py
+++ pythonFinance.py
@@ -6,9 +6,18 @@ import pandas_datareader.data as web
 
 style.use("ggplot")
 
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2017, 12, 31)
+# - Generate TESLA stock data using Yahoo web service
+# start = dt.datetime(2000, 1, 1)
+# end = dt.datetime(2017, 12, 31)
+# df = web.DataReader("TSLA", "yahoo", start, end)
 
-df = web.DataReader("TSLA", "yahoo", start, end)
+# - Export dataframe to csv format
+# df.to_csv("TESLA.csv")
 
-print(df.head())
+# - Read TESLA stock data from created CSV file
+df = pd.read_csv("TESLA.csv", parse_dates = True, index_col = 0)
+
+# - Analyze & Plot Stock Data
+# print(df.head())
+df.plot()
+plt.show()
