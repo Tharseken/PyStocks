f8cfdcb979e8e8f71d4dfff094694f39728d2e82
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2018-11-14 19:29:09 -0500

    Created moving average data set. Manipulated stock data information

--- pythonFinance.py
+++ pythonFinance.py
@@ -19,5 +19,16 @@ df = pd.read_csv("TESLA.csv", parse_dates = True, index_col = 0)
 
 # - Analyze & Plot Stock Data
 # print(df.head())
-df.plot()
+
+df["100ma"] = df["Adj Close"].rolling(window = 100, min_periods = 0).mean()
+
+print(df.head())
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex = ax1)
+
+ax1.plot(df.index, df["Adj Close"])
+ax1.plot(df.index, df["100ma"])
+ax2.bar(df.index, df["Volume"])
+
 plt.show()
