4cfaeacef679adc586c1200ab4c5682a3cdf9822
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2018-11-14 22:46:53 -0500

    Created and plotted candlestick plot of Tesla's OHLC

--- pythonFinance.py
+++ pythonFinance.py
@@ -1,6 +1,8 @@
 import datetime as dt
 import matplotlib.pyplot as plt 
 from matplotlib import style
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 
@@ -17,18 +19,29 @@ style.use("ggplot")
 # - Read TESLA stock data from created CSV file
 df = pd.read_csv("TESLA.csv", parse_dates = True, index_col = 0)
 
+df_ohlc = df["Adj Close"].resample("10D").ohlc()
+df_volume = df["Volume"].resample("10D").sum()
+
+df_ohlc.reset_index(inplace=True)
+
+df_ohlc["Date"] = df_ohlc["Date"].map(mdates.date2num)
+
+# print(df_ohlc.head())
+
 # - Analyze & Plot Stock Data
 # print(df.head())
 
-df["100ma"] = df["Adj Close"].rolling(window = 100, min_periods = 0).mean()
-
-print(df.head())
+# - Create a moving average column in dataframe
+# df["100ma"] = df["Adj Close"].rolling(window = 100, min_periods = 0).mean()
 
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex = ax1)
+ax1.xaxis_date()
 
-ax1.plot(df.index, df["Adj Close"])
-ax1.plot(df.index, df["100ma"])
-ax2.bar(df.index, df["Volume"])
+# - Create candlestick graph
+candlestick_ohlc(ax1, df_ohlc.values, width = 2, colorup= "g")
 
+ax2.filzl_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+
+# - Render the stock data plot
 plt.show()
