a47e89c3f06a26784dd1ac40577bd001b8b11589
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2018-11-15 22:12:22 -0500

    Created function to automatically scrape and save all S&P500 stocks to CSV files

--- sp500.py
+++ sp500.py
@@ -1,5 +1,9 @@
 # Import Statements
 import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web 
 import pickle
 import requests
 
@@ -26,5 +30,29 @@ def save_sp500_tickers():
         
     return tickers
 
+# save_sp500_tickers()
 
-save_sp500_tickers()
+
+def get_data_from_yahoo (reload_sp500 = False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open ("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000,1,1)
+    end = dt.datetime(2018,10,31)
+
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, "yahoo", start, end)
+            df.to_csv("stock_dfs/{}.csv".format(ticker))
+        else :
+            print("Already have {}".format(ticker))
+
+
+get_data_from_yahoo()
