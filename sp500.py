84c5d7c80b0ca1b1ed5294a696616e9d041641da
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2018-11-18 18:48:25 -0500

    Created and plotted a S&P500 company correlation dataframe. Created a heatmap to display the correlations between companies

--- sp500.py
+++ sp500.py
@@ -3,9 +3,14 @@ import bs4 as bs
 import datetime as dt
 import os
 import pandas as pd
+import numpy as np
 import pandas_datareader.data as web 
+import matplotlib.pyplot as plt 
+from matplotlib import style
 import pickle
 import requests
+
+style.use("ggplot")
 
 # Scrape tickers from Wikipedia
 def save_sp500_tickers():
@@ -78,4 +83,41 @@ def compile_data () :
     main_df.to_csv("sp500_joined_closes.csv")
 
 
-compile_data();
+def visualize_data():
+    df = pd.read_csv("sp500_joined_closes.csv")
+
+    # - Plot an individual stock's data
+    # df["AAPL"].plot()
+    # plt.show()
+
+    # - Create a correlation dataframe using pandas correlation function
+    df_corr = df.corr()
+    # print(df_corr.head())
+
+    # - Setup a heatmap for the correlation data
+    data = df_corr.values
+    figure = plt.figure()
+    ax = figure.add_subplot(1, 1, 1)
+
+    heatmap = ax.pcolor(data, cmap = plt.cm.RdYlGn)
+    figure.colorbar = (heatmap)
+
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+
+    plt.xticks(rotation = 90)
+    heatmap.set_clim(-1,1)
+
+    plt.tight_layout()
+    plt.show()
+
+visualize_data()
