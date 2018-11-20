37776a3e738a8608549599d639494bd8398c0d65
Tharseken Navajeevayokan <tharseken.n@gmail.com>
2018-11-19 23:02:13 -0500

    Created labels fro  S&P 500 company stock data for future machine learning algorithm implementation

--- preprocessData.py
+++ preprocessData.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from collections import Counter
 import pickle
 
 def process_data_for_labels(ticker):
@@ -25,5 +26,31 @@ def buy_sell_hold(*args):
             return 1
         if col < -requirement:
             return -1
-
     return 0
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df["{}_target".format(ticker)] = list(map(buy_sell_hold, df["{}_1d".format(ticker)], df["{}_2d".format(ticker)], df["{}_3d".format(ticker)], df["{}_4d".format(ticker)], df["{}_5d".format(ticker)], df["{}_6d".format(ticker)], df["{}_7d".format(ticker)]))
+
+    vals = df["{}_target".format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print("Data spread: ", Counter(str_vals))
+    df.fillna(0, inplace = True)
+    
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna (inplace = True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace = True)
+
+    X = df_vals.values
+    y = df["{}_target".format(ticker)].values
+
+    return X,y,df
+
+
+
+extract_featuresets("XOM")
